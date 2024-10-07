18e4773eebf9fad03ac2ee451cc74e9d607ec994
Sameer Ahamed <122287404+sameer-del@users.noreply.github.com>
2024-10-07 17:54:56 +0530

    creating routing and two pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import Login from "./Login";
-
+import React from "react";
+import { Link } from "react-router-dom";
+import "./app.css";
 function App() {
   return (
     <>
-      <h1>student management</h1>
-      <Login />
+      <div className="background">
+        <h1>
+          Welcome to the <br /> Student Management System
+        </h1>
+        <h3>Please login or signup to continue.</h3>
+        <div className="buttons">
+          <button>
+            <Link to="/login">Login</Link>
+          </button>
+          <button>
+            <Link to="/signup">Signup</Link>
+          </button>
+        </div>
+      </div>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import App from "./App";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  </Router>
+);
