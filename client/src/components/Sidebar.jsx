64b84940c8cfc7b090228b7d9384a788e86ec419
harshathwindows <harshathwindows@gmail.com>
2024-10-09 10:00:12 +0530

    added h1 element in sidebar component

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Sidebar = () => {
-  return <div>build your knowledge</div>;
+  return <div>build your knowledges!
+    <h1>hello</h1>
+  </div>;
 };
 
 export default Sidebar;
